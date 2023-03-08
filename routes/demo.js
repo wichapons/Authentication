7280ca0c5cab1ca13b208737ffbb4ea7df0476d2
Wichapon V <wichapon@gmail.com>
2023-03-08 21:49:55 +0700

    Convert 'signup' route to async and add user data to database

--- routes/demo.js
+++ routes/demo.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res) {
   res.render('welcome');
 });
 
-router.get('/signup', function (req, res) {
+router.get('/signup', async function (req, res) {
   res.render('signup');
 });
 
@@ -16,7 +16,18 @@ router.get('/login', function (req, res) {
   res.render('login');
 });
 
-router.post('/signup', async function (req, res) {});
+router.post('/signup', async function (req, res) {
+  const userData = req.body;
+  const email = userData.email;
+  const confirmEmail = userData['email-confirm']; //could not use userData.email-confirm because there is a dash sign. userData['email-confirm'] =  userData.email-confirm 
+  const password = userData.password;
+  const user = {
+    email: email,
+    password:password,
+  };
+  await db.getDb().collection('users').insertOne(user);
+  res.redirect('/login');
+});
 
 router.post('/login', async function (req, res) {});
 
