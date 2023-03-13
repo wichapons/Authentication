515044759b9857cda1204820facae730d53be80e
Wichapon V <wichapon@gmail.com>
2023-03-13 16:30:50 +0700

    add global variable to get the data whether to show log out admin profile or not

--- app.js
+++ app.js
@@ -32,6 +32,22 @@ app.use(session({
   store: sessionStore
 }));
 
+app.use(async function(req,res,next){
+  const user = req.session.user
+  const isAuth = req.session.isAuthenticated;
+  
+
+  if(!user || !isAuth){
+    return next();
+  }
+  const userDoc = await db.getDb().collection('users').findOne({_id:user.id})
+  const isAdmin = userDoc.isAdmin;
+
+  res.locals.isAuth = isAuth;
+  res.locals.isAdmin = isAdmin
+  next();
+});
+
 app.use(demoRoutes);
 
 app.use(function(error, req, res, next) {
